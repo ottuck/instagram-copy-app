057dbfc913573e4f6ad3f147e70d4e6961409862
SangHyun Kim <vison901@gmail.com>
2023-08-13 01:37:00 +0900

    serch page modal veiw impl

--- src/components/Stories2.js
+++ src/components/Stories2.js
@@ -1,117 +1,117 @@
-import { ScrollView, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
-import { Entypo } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+// import { ScrollView, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
+// import React from 'react'
+// import { Entypo } from '@expo/vector-icons';
+// import { useNavigation } from '@react-navigation/native';
 
-// 스토리보드 가데이터
-const storyInfo = [
-    {
-        id: 1,
-        name: 'john',
-        image: require('../../assets/images/userProfile.jpeg')
-    },
-    {
-        id: 2,
-        name: 'tony',
-        image: require('../../assets/images/profile1.jpeg')
-    },
-    {
-        id: 3,
-        name: 'kan',
-        image: require('../../assets/images/profile2.jpeg')
-    },
-    {
-        id: 4,
-        name: 'July',
-        image: require('../../assets/images/profile3.jpeg')
-    },
-    {
-        id: 5,
-        name: 'daniel',
-        image: require('../../assets/images/profile4.jpeg')
-    },
-    {
-        id: 6,
-        name: 'Happy',
-        image: require('../../assets/images/profile5.jpeg')
-    }
-]
+// // 스토리보드 가데이터
+// const storyInfo = [
+//     {
+//         id: 1,
+//         name: 'john',
+//         image: require('../../assets/images/userProfile.jpeg')
+//     },
+//     {
+//         id: 2,
+//         name: 'tony',
+//         image: require('../../assets/images/profile1.jpeg')
+//     },
+//     {
+//         id: 3,
+//         name: 'kan',
+//         image: require('../../assets/images/profile2.jpeg')
+//     },
+//     {
+//         id: 4,
+//         name: 'July',
+//         image: require('../../assets/images/profile3.jpeg')
+//     },
+//     {
+//         id: 5,
+//         name: 'daniel',
+//         image: require('../../assets/images/profile4.jpeg')
+//     },
+//     {
+//         id: 6,
+//         name: 'Happy',
+//         image: require('../../assets/images/profile5.jpeg')
+//     }
+// ]
 
-const Stories2 = () => {
-    const navigation = useNavigation();
+// const Stories2 = () => {
+//     const navigation = useNavigation();
 
-    return (
-        <ScrollView
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-            style={{ paddingVertical: 20 }}
-        >
-            {storyInfo.map((data, index) => {
-                return (
-                    <TouchableOpacity
-                        key={index}
-                        // 누르면 'Status' 값과 name, imaage를 가지고 이동하게 
-                        onPress={() => navigation.push('Status', {
-                            name: data.name,
-                            image: data.image,
-                        })}
-                    >
-                        <View style={styles.storyIconContainer}>
-                            {data.id === 1 ? (
-                                <View style={styles.storyIconPlusButton}>
-                                    <Entypo name="circle-with-plus"
-                                        style={styles.storyPlusButton} />
-                                </View>
-                            ) : null}
-                            <View style={styles.storyIconCircle}>
-                                <Image source={data.image}
-                                    style={styles.storyImage} />
-                            </View>
-                            <Text></Text>
-                        </View>
-                    </TouchableOpacity>
-                )
-            })}
-        </ScrollView>
-    )
-}
-export default Stories2;
+//     return (
+//         <ScrollView
+//             horizontal={true}
+//             showsHorizontalScrollIndicator={false}
+//             style={{ paddingVertical: 20 }}
+//         >
+//             {storyInfo.map((data, index) => {
+//                 return (
+//                     <TouchableOpacity
+//                         key={index}
+//                         // 누르면 'Status' 값과 name, imaage를 가지고 이동하게 
+//                         onPress={() => navigation.push('Status', {
+//                             name: data.name,
+//                             image: data.image,
+//                         })}
+//                     >
+//                         <View style={styles.storyIconContainer}>
+//                             {data.id === 1 ? (
+//                                 <View style={styles.storyIconPlusButton}>
+//                                     <Entypo name="circle-with-plus"
+//                                         style={styles.storyPlusButton} />
+//                                 </View>
+//                             ) : null}
+//                             <View style={styles.storyIconCircle}>
+//                                 <Image source={data.image}
+//                                     style={styles.storyImage} />
+//                             </View>
+//                             <Text></Text>
+//                         </View>
+//                     </TouchableOpacity>
+//                 )
+//             })}
+//         </ScrollView>
+//     )
+// }
+// export default Stories2;
 
-const styles = StyleSheet.create({
-    storyIconContainer: {
-        flexDirection: 'column',
-        paddingHorizontal: 8,
-        position: 'relative'
-    },
-    storyIconPlusButton: {
-        position: 'absolute',
-        bottom: 15,
-        right: 10,
-        zIndex: 1,
-        borderRadius: 10,
-        overflow: 'hidden'
-    },
-    storyPlusButton: {
-        fontSize: 20,
-        color: 'blue',
-        backgroundColor: 'white',
-        borderRadius: 100,
-    },
-    storyIconCircle: {
-        width: 68,
-        height: 68,
-        backgroundColor: 'white',
-        borderRadius: 100,
-        borderWidth: 1.8,
-        borderColor: '#c13584',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    storyImage: {
-        resizeMode: 'cover',
-        width: '92%',
-        height: '92%',
-        borderRadius: 100,
-        borderColor: 'orange'
-    }
-})
+// const styles = StyleSheet.create({
+//     storyIconContainer: {
+//         flexDirection: 'column',
+//         paddingHorizontal: 8,
+//         position: 'relative'
+//     },
+//     storyIconPlusButton: {
+//         position: 'absolute',
+//         bottom: 15,
+//         right: 10,
+//         zIndex: 1,
+//         borderRadius: 10,
+//         overflow: 'hidden'
+//     },
+//     storyPlusButton: {
+//         fontSize: 20,
+//         color: 'blue',
+//         backgroundColor: 'white',
+//         borderRadius: 100,
+//     },
+//     storyIconCircle: {
+//         width: 68,
+//         height: 68,
+//         backgroundColor: 'white',
+//         borderRadius: 100,
+//         borderWidth: 1.8,
+//         borderColor: '#c13584',
+//         justifyContent: 'center',
+//         alignItems: 'center'
+//     },
+//     storyImage: {
+//         resizeMode: 'cover',
+//         width: '92%',
+//         height: '92%',
+//         borderRadius: 100,
+//         borderColor: 'orange'
+//     }
+// })
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,17 +1,108 @@
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  View,
+  StatusBar,
+  ScrollView,
+  Dimensions,
+  Image,
+  Text,
+  SafeAreaView,
+  Platform
+} from 'react-native';
+
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Feather from '@expo/vector-icons/Feather';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
 import SearchInput from '../components/SearchInput';
 import SearchContent from '../components/SearchContent';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 const Search = () => {
+  const [image, setImage] = useState(null);
+
+  const getData = data => {
+    setImage(data);
+  };
+
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
+
+  const statusBarHeight = getStatusBarHeight();
+
   return (
-    <SafeAreaView>
-      <ScrollView>
+    <SafeAreaView
+      style={{
+        width: '100%',
+        backgroundColor: 'white',
+      }}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {/* <SearchBox /> */}
         <SearchInput />
-        <SearchContent />
+        {/* <SearchContent /> */}
+        <SearchContent data={getData} />
       </ScrollView>
+      {image ? (
+        <View
+          style={{
+            top: Platform.OS === 'ios' ? statusBarHeight : 0 ,
+            position: 'absolute',
+            zIndex: 1,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(52,52,52,0.8)',
+          }}>
+          <StatusBar backgroundColor="#525252" barStyle="dark-content" />
+          <View
+            style={{
+              position: 'absolute',
+              top: windowHeight / 6,
+              left: windowWidth / 18,
+              backgroundColor: 'white',
+              width: '90%',
+              height: 465,
+              borderRadius: 15,
+              zIndex: 1,
+            }}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                paddingVertical: 10,
+                paddingHorizontal: 15,
+              }}>
+              <Image
+                source={image}
+                style={{
+                  width: 30,
+                  height: 30,
+                  borderRadius: 100,
+                }}
+              />
+              <View style={{ paddingLeft: 8 }}>
+                <Text style={{ fontSize: 12, fontWeight: '600' }}>
+                  친구 아이디
+                </Text>
+              </View>
+            </View>
+            <Image source={image} style={{ width: '100%', height: '80%' }} />
+            <View
+              style={{
+                justifyContent: 'space-around',
+                width: '100%',
+                flexDirection: 'row',
+                alignItems: 'center',
+                padding: 8,
+              }}>
+              <Ionicons name="ios-heart-outline" style={{ fontSize: 26 }} />
+              <Ionicons name="ios-person-circle-outline" style={{ fontSize: 26 }} />
+              <Feather name="navigation" style={{ fontSize: 26 }} />
+            </View>
+          </View>
+        </View>
+      ) : null}
     </SafeAreaView>
-  )
-}
+  );
+};
 
 export default Search;
