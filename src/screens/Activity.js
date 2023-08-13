a49b86381f23362c0c86cd9b309b8dafe5b8a3c1
SangHyun Kim <vison901@gmail.com>
2023-08-13 17:53:26 +0900

    impement Activity view

--- src/screens/Activity.js
+++ src/screens/Activity.js
@@ -1,12 +1,43 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { Text, ScrollView, SafeAreaView } from 'react-native';
+import { FriendsProfileData } from '../components/Database';
+import ActivityThisWeek from '../components/ActivityThisWeek';
+import ActivityPrevious from '../components/ActivityPrevious';
+import ActivityRecommend from '../components/ActivityRecommend';
 
 const Activity = () => {
   return (
-    <View>
-      <Text>Activity</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{ width: '100%', backgroundColor: 'white' }}>
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: 'bold',
+          borderBottomWidth: 0.5,
+          borderBottomColor: '#DEDEDE',
+          padding: 10,
+        }}>
+        활동
+      </Text>
+      <ScrollView style={{ margin: 10 }} showsVerticalScrollIndicator={false}>
 
-export default Activity
+        <ActivityThisWeek />
+
+        <Text style={{ fontWeight: 'bold' }}>이전 활동</Text>
+        {FriendsProfileData.slice(3, 6).map((data, index) => (
+          <ActivityPrevious data={data} key={index} />
+        ))}
+
+
+        <Text style={{ fontWeight: 'bold', paddingVertical: 10 }}>
+          회원님을 위한 추천
+        </Text>
+        {FriendsProfileData.slice(6.12).map((data, index) => (
+          <ActivityRecommend data={data} key={index} />
+        ))}
+
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Activity;
